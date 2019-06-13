60e23ac05724db878b46824e72aef142a34f9735
Sonic Xiang <xiangsongxa@gmail.com>
2019-06-13 22:51:10 +0800

    Fix asset-manifest ref

--- scripts/update-chrome-extensions-prop.js
+++ scripts/update-chrome-extensions-prop.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 let manifest = JSON.parse(fs.readFileSync('build/manifest.json', 'utf8'));
 const hashes = JSON.parse(fs.readFileSync('build/asset-manifest.json', 'utf8'));
-manifest.background.scripts[0] = hashes['background.js'];
-manifest.background.scripts[1] = hashes['runtime.js'];
+manifest.background.scripts[0] = hashes.files['background.js'];
+manifest.background.scripts[1] = hashes.files['runtime.js'];
 if (process.env.PUBLISH_BUILD !== 'true') {
     manifest.content_security_policy = "script-src 'self' 'unsafe-eval'; object-src 'self'";
 }
